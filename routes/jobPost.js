5607f11e9901e04a80cb09865fe2353ed3374d6a
Ubuntu <ubuntu@ip-172-31-0-197.ap-south-1.compute.internal>
2025-09-05 15:10:37 +0000

    url in studentjobposts, express json limit 200mb, file saving local name fix

--- routes/jobPost.js
+++ routes/jobPost.js
@@ -12,7 +12,13 @@ const storagevideo = multer.diskStorage({
     cb(null, path.join(__dirname, "../uploads")); // uploads folder
   },
   filename: (req, file, cb) => {
-    let filename = req.body?.fileName;
+console.log("-------",req.body);
+     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    const safeBaseName = path
+      .basename(file.originalname, ext)
+      .replace(/\s+/g, "_");
+    const filename = `${safeBaseName}-${uniqueSuffix}.webm`;
     cb(null, filename);
   },
 });
@@ -25,12 +31,21 @@ router.post(
   uploadvideo.single("video"),
   (req, res) => {
     console.log(req.file, "file Name:", req.body.fileName);
+console.time("Video Upload");
     if (!req.file) return res.status(400).send("No file uploaded.");
-    res.send({
+console.timeEnd("Video Upload");
+    console.log("✅ File saved to disk:", req.file.path);
+
+    // Prepare the response data
+    const responseData = {
       message: "Video uploaded successfully",
-      fileName: req.body.fileName,
-      path: `uploads/${req.body.fileName}`, // can use for video src
-    });
+      fileName: req.file.filename,
+      path: `uploads/${req.file.filename}`,
+    };
+
+    // Set content type and end the response with JSON
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(responseData));
   }
 );
 
